7882f65abe0451ee32dad284f3bb296637470465
Scott Sickles <scottsickles@gmail.com>
2022-01-28 11:25:27 -0500

    updated the module outputs to match our convention

--- domain.tf
+++ domain.tf
@@ -10,12 +10,12 @@ resource "aws_iam_service_linked_role" "this" {
 }
 
 resource "aws_elasticsearch_domain" "this" {
-  domain_name = local.resource_name
-  tags        = local.tags
+  domain_name           = local.resource_name
+  tags                  = local.tags
   elasticsearch_version = "OpenSearch_${var.opensearch_version}"
 
   domain_endpoint_options {
-    enforce_https = true
+    enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
@@ -25,11 +25,11 @@ resource "aws_elasticsearch_domain" "this" {
 
   cluster_config {
     dedicated_master_enabled = var.dedicated_master_count != 0
-    dedicated_master_count = var.dedicated_master_count
-    dedicated_master_type = var.dedicated_master_type
+    dedicated_master_count   = var.dedicated_master_count
+    dedicated_master_type    = var.dedicated_master_type
 
     instance_count = var.instance_count
-    instance_type = var.instance_type
+    instance_type  = var.instance_type
 
     zone_awareness_enabled = local.zone_count > 1
     zone_awareness_config {
@@ -39,7 +39,7 @@ resource "aws_elasticsearch_domain" "this" {
 
   vpc_options {
     security_group_ids = [aws_security_group.this.id]
-    subnet_ids = slice(local.private_subnet_ids, 0, var.instance_count)
+    subnet_ids         = slice(local.private_subnet_ids, 0, var.instance_count)
   }
 
   depends_on = [aws_iam_service_linked_role.this]
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
-output "domain_arn" {
+output "db_arn" {
   value       = aws_elasticsearch_domain.this.arn
   description = "string ||| ARN of the OpenSearch domain"
 }
 
-output "domain_endpoint" {
-  value       = aws_elasticsearch_domain.this.endpoint
-  description = "string ||| The endpoint URL to submit index, search, and data upload requests"
+output "db_protocol" {
+  value       = "https"
+  description = "string ||| The protocol used to connect to the OpenSearch domain"
 }
 
-output "domain_security_group_id" {
+output "db_hostname" {
+  value       = aws_elasticsearch_domain.this.endpoint
+  description = "string ||| The hostname of the OpenSearch domain"
+}
+
+output "db_port" {
+  value       = local.port
+  description = "number ||| The port used to connect to the OpenSearch domain"
+}
+
+output "db_security_group_id" {
   value       = aws_security_group.this.id
   description = "string ||| The ID of the security group attached to the domain"
 }
