c0cafb80ec90cbc52a64344fc01380214e60d951
Brad Sickles <brad.sickles@gmail.com>
2022-05-31 12:28:21 -0400

    Standardize

--- aws.tf
+++ aws.tf
@@ -1,1 +1,2 @@
 data "aws_region" "this" {}
+data "aws_caller_identity" "this" {}
--- policy.tf
+++ policy.tf
@@ -1,16 +1,14 @@
-data "aws_caller_identity" "this" {}
-
 data "aws_iam_policy_document" "this" {
   statement {
     sid     = "AllowAllCallers"
     effect  = "Allow"
     actions = ["es:*"]
+
     principals {
       identifiers = ["*"]
       type        = "*"
     }
-    resources = [
-      "arn:aws:es:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:domain/${local.resource_name}/*"
-    ]
+
+    resources = ["arn:aws:es:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:domain/${local.resource_name}/*"]
   }
 }
